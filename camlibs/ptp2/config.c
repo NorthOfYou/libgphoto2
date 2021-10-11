54ba1c103625edbb1d3c5ae5c7c98445896235f3
Ben Govero <ben@witharsenal.com>
2021-10-11 11:30:26 -0500

    Don't attempt to update the focusmode when change manualfocus position in sony

--- camlibs/ptp2/config.c
+++ camlibs/ptp2/config.c
@@ -8997,8 +8997,6 @@ _put_Sony_ManualFocus(CONFIG_PUT_ARGS)
 		xpropval.u16 = 2;
 		C_PTP (ptp_sony_setdevicecontrolvalueb (params, 0xd2d2, &xpropval, PTP_DTC_UINT16));
 		*/
-		xpropval.u16 = 2;
-		C_PTP (ptp_sony_setdevicecontrolvalueb (params, 0xd2d2, &xpropval, PTP_DTC_UINT16));
 		if(val <= -7) xpropval.u16 = 0xFFFF - 6;
 		else if(val <= -6.0) xpropval.u16 = 0xFFFF - 5;
 		else if(val <= -5.0) xpropval.u16 = 0xFFFF - 4;
@@ -9015,9 +9013,6 @@ _put_Sony_ManualFocus(CONFIG_PUT_ARGS)
 		else if(val <= 7.0) xpropval.u16 = 7;
 		else xpropval.u16 = 0;
 		C_PTP (ptp_sony_setdevicecontrolvalueb (params, PTP_DPC_SONY_NearFar, &xpropval, PTP_DTC_UINT16));
-	} else {
-		xpropval.u16 = 1;
-		C_PTP (ptp_sony_setdevicecontrolvalueb (params, 0xd2d2, &xpropval, PTP_DTC_UINT16));
 	}
 
 	return PUT_OK;
