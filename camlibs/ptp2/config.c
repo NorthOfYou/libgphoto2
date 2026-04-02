cca07996930b0e70cd6de941f1c9715fe8ba11fd
Ryan Stout <ryanstout@gmail.com>
2026-04-02 15:49:16 -0600

    Add Nikon Z5 ii

--- camlibs/ptp2/config.c
+++ camlibs/ptp2/config.c
@@ -11942,6 +11942,7 @@ static struct menu menus[] = {
 	{ N_("Capture Settings"),           "capturesettings",  0x4b0,  0x044b, nikon_z6_capture_settings,      NULL,   NULL }, /* Z7_2 guessed */
 	{ N_("Capture Settings"),           "capturesettings",  0x4b0,  0x0450, nikon_z9_capture_settings,      NULL,   NULL }, /* Z9 */
 	{ N_("Capture Settings"),           "capturesettings",  0x4b0,  0x0451, nikon_z9_capture_settings,      NULL,   NULL }, /* Z8 */
+	{ N_("Capture Settings"),           "capturesettings",  0x4b0,  0x0456, nikon_z9_capture_settings,      NULL,   NULL }, /* Z5_2 */
 	{ N_("Capture Settings"),           "capturesettings",  0x4b0,  0x044c, nikon_z6_capture_settings,      NULL,   NULL }, /* Z6_2 guessed */
 	{ N_("Capture Settings"),           "capturesettings",  0x4b0,  0x0454, nikon_z9_capture_settings,      NULL,   NULL }, /* Z6_3 */
 	{ N_("Capture Settings"),           "capturesettings",  0x4b0,  0,      nikon_generic_capture_settings, NULL,   NULL },
--- camlibs/ptp2/library.c
+++ camlibs/ptp2/library.c
@@ -1769,6 +1769,9 @@ static struct {
 	/* Thomas Schaad */
 	{"Nikon:Z5",                      0x04b0, 0x0448, PTP_CAP|PTP_CAP_PREVIEW},
 
+	/* Nikon Z5 II, USB PID 0x0456, iProduct "NIKON DSC Z5_2" */
+	{"Nikon:Z5_2",                    0x04b0, 0x0456, PTP_CAP|PTP_CAP_PREVIEW},
+
 	/* Fahrion <fahrion.2600@gmail.com> */
 	{"Nikon:Z7_2",                	  0x04b0, 0x044b, PTP_CAP|PTP_CAP_PREVIEW},
 
